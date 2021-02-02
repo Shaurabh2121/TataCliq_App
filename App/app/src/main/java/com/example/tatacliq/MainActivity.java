56d1a6f3a2bc928e34942411556439e92b0aa65c
Sibtain Ahmad <sibtainahmad976@gmail.com>
2021-02-03 03:49:44 +0530

    Create Splash Screen

--- App/app/src/main/java/com/example/tatacliq/MainActivity.java
+++ App/app/src/main/java/com/example/tatacliq/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.tatacliq;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView mTvV1, mTvV2, mTvV3;
+    private ImageView mIvCliq;
+    private Handler handler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTvV1 = findViewById(R.id.v1);
+        mTvV2 = findViewById(R.id.v2);
+        mTvV3 = findViewById(R.id.v3);
+        mIvCliq = findViewById(R.id.ivCliq);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+
+    }
     }
 }
